5b24eab17801a7f5ebcdcefbbbca170c1c3d4c01
m33tang <m33tang@uwaterloo.ca>
2024-05-25 02:40:16 -0400

    update packages? idek

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const fs = require('node:fs');
 const path = require('node:path');
+
+
+
+
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 
 const { clientId, guildId, token, dburi } = require('./config.json');
