b55f1e0842a7889a98cb6e10cff372c63061868e
Matthew Tang <matttang27@gmail.com>
2024-07-04 02:09:06 -0400

    Changed @config path to refer to the entire folder

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,6 @@
 require('module-alias-jest/register')
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
+const { clientId, guildId, token } = require('@config/config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('module-alias-jest/register')
 const MatthewClient = require('@client');
-const config = require('@config');
+const config = require('@config/config.json');
 const client = new MatthewClient(config,true);
 client.login();
 
